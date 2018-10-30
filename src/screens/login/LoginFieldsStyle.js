4cbe045bac0a94a4b984b62ddb165b161c90f388
g.tsaousidis <giorgostsd@gmail.com>
2018-10-31 00:25:23 +0200

    Create loginfields style file

---
+++ src/screens/login/LoginFieldsStyle.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  fieldContainer: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    margin: 15,
+  },
+  text: {
+    fontSize: 18,
+  },
+  loginInput: {
+    flex: 1,
+    backgroundColor: '#EAEAEB',
+    marginLeft: 15,
+    marginRight: 5,
+    fontSize: 14,
+    borderRadius: 2,
+  },
+});
