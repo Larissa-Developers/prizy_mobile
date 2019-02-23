2b15b4356a8aded77d9d4fc3e8659a3496eb9f22
Tsolis Dimitris Sotiris <dimitristls@gmail.com>
2019-02-23 14:44:20 +0200

    Reactor RetryView to be a function

--- src/components/commons/RetryView.js
+++ src/components/commons/RetryView.js
@@ -2,19 +2,14 @@ import * as React from 'react';
 import { Button, View, Text } from 'native-base';
 import PropTypes from 'prop-types';
 
-class RetryView extends React.Component {
-  render() {
-    const { style, message, buttonText, onPress } = this.props;
-    return (
-      <View style={style}>
-        <Text>{message}</Text>
-        <Button transparent style={{ alignSelf: 'center' }} onPress={onPress}>
-          <Text>{buttonText || 'Retry'}</Text>
-        </Button>
-      </View>
-    );
-  }
-}
+const RetryView = ({ style, message, buttonText, onPress }) => (
+  <View style={style}>
+    <Text>{message}</Text>
+    <Button transparent style={{ alignSelf: 'center' }} onPress={onPress}>
+      <Text>{buttonText || 'Retry'}</Text>
+    </Button>
+  </View>
+);
 
 RetryView.propTypes = {
   style: PropTypes.object,
